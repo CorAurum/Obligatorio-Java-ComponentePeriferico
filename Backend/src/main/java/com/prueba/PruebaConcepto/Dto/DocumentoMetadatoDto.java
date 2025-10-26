780f62c650268dc471566c43eeef1e5bca9f3570
Fabricio <Fabriciogarcia20@gmail.com>
2025-10-26 16:45:35 -0300

    minor edits 2

--- Backend/src/main/java/com/prueba/PruebaConcepto/Dto/DocumentoMetadatoDto.java
+++ Backend/src/main/java/com/prueba/PruebaConcepto/Dto/DocumentoMetadatoDto.java
@@ -1,6 +1,10 @@
 package com.prueba.PruebaConcepto.Dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record DocumentoMetadatoDto(
+
+        @JsonProperty("idDocumentoOrigen")
         Long idDocumento,
         String area,
         String fechaCreacion,
