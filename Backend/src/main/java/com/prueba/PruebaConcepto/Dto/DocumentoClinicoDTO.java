a39ab58860a783ec0f7c921d2dfa59fd5451a9aa
Fabricio <Fabriciogarcia20@gmail.com>
2025-11-19 22:02:49 -0300

    Fix DocumentoClinicoDTO getters and setters

--- Backend/src/main/java/com/prueba/PruebaConcepto/Dto/DocumentoClinicoDTO.java
+++ Backend/src/main/java/com/prueba/PruebaConcepto/Dto/DocumentoClinicoDTO.java
@@ -1,25 +1,19 @@
 package com.prueba.PruebaConcepto.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class DocumentoClinicoDTO {
-
     private LocalDateTime fechaCreacion;
     private String area;
     private String descripcion;
     private String documentoId;
     private String profesionalNombre;
     private String profesionalApellido;
-
-    public DocumentoClinicoDTO(LocalDateTime fechaCreacion, String area, String descripcion,
-                               String documentoId, String profesionalNombre, String profesionalApellido) {
-        this.fechaCreacion = fechaCreacion;
-        this.area = area;
-        this.descripcion = descripcion;
-        this.documentoId = documentoId;
-        this.profesionalNombre = profesionalNombre;
-        this.profesionalApellido = profesionalApellido;
-    }
-
-    // getters y setters
 }
